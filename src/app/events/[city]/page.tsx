8137d77f6fcc1bc22cf4dc8a568435c015709ddd
aman304gupta <aman304gupta@gmail.com>
2024-07-02 19:24:55 -0700

    Added Events page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,31 +1,38 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
-import React from "react";
+import React, { Suspense } from "react";
+import Loading from "./loading";
+import { capitalize } from "@/lib/utils";
+import { Metadata } from "next";
 
-type EventsPageProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-export default async function Events({ params }: EventsPageProps) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
-  );
-
-  const events: EventoEvent[] = await response.json();
-
+export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
+
+  return {
+    title: city === "all" ? "All Events" : `Events in ${capitalize(city)}`,
+    description: `Find the best events in ${city}`,
+  };
+}
+
+export default async function Events({ params }: Props) {
+  const city = params.city;
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === "all" && "All Events"}
-        {city !== "all" &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -17,14 +17,14 @@ export default function EventCard({ event }: EventCardProps) {
       <section
         className="w-full h-full flex flex-col 
          bg-white/[3%] rounded-xl overflow-hidden
-    relative transition hover:scale-105 active:scale-[1.02]"
+    relative state-effects"
       >
         <Image
           src={event.imageUrl}
           alt={event.name}
           width={500}
           height={280}
-          className="h-[60%] object-fit"
+          className="h-[60%] object-cover"
         />
         <div className="flex flex-col flex-1 justify-center items-center">
           <h2 className="text-2xl font-semibold">{event.name}</h2>
